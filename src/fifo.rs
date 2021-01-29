9afeec981750032ac04bdff4f252b76c66995c20
lechub <lechub@tlen.pl>
2021-01-29 13:09:31 +0100

    aaa

---
+++ src/fifo.rs
@@ -0,0 +1,91 @@
+
+
+//struct Fifo<T: ?Sized>{
+//	max_size: const usize,
+//	data: T,
+//	head: usize,
+//	tail: usize,
+//}
+
+
+pub struct Fifo2<T>{
+	head: usize,
+	tail: usize,
+	data: T,
+}
+
+impl<u8> Fifo2<u8>{
+	pub const fn new(aa:u8) ->Self{
+		Fifo2{
+			head: 0,
+			tail: 0,
+			data: aa,
+		}
+	}
+}
+
+impl<[u8; usize]> Fifo2<[u8; usize]>{
+	pub const fn new(size: usize) -> Self{
+		Fifo2{
+			head: 0,
+			tail: 0,
+			data: [0; size],
+		}
+	}
+}
+
+
+
+pub enum FifoError{
+		BufferEmpty,
+		BufferFull,
+	}
+	
+pub trait Fifo{	
+	fn put(znak :u8) -> Result<bool, FifoError>;
+	fn get() -> Option<u8>;
+	fn capacity()-> usize;
+	fn used_bytes()-> usize;
+	fn available_bytes()-> usize;
+}
+
+struct FifoStr{
+	head: usize,
+	tail: usize,
+	data: [u8],
+}
+
+//macro_rules! make_fifo {
+//    ($size:literal) => {{
+//		Fifo{
+//			max_size: $size,
+//			data: [0; $size +1],
+//			head: 0,
+//			tail: 0,
+//		}        
+//    }}
+//}
+
+//impl Default for Fifo<T: ?Sized> {
+//    fn default(size: usize) -> Fifo {
+//        make_fifo!(size)
+//    }
+//}
+
+impl FifoStr  {
+	
+    pub fn new(size: usize) -> FifoStr {
+       //Self::default()
+		//let tab =  [0; size];
+		
+		FifoStr{
+			head: 0,
+			tail: 0,
+			data: [0;size],
+		}
+
+    }
+
+}
+
+
---
+++ src/main.rs
@@ -0,0 +1,83 @@
+// std and main are not available for bare metal software
+#![no_std]
+#![no_main]
+#[warn(non_snake_case)]
+
+//use cortex_m_rt::entry; // The runtime
+//use embedded_hal::digital::v2::OutputPin; // the `set_high/low`function
+//use stm32f1xx_hal::{delay::Delay, pac, prelude::*}; // STM32F1 specific functions
+//use stm32f0::stm32f0x1;
+
+
+
+extern crate cortex_m;
+
+extern crate panic_halt; //extern crate panic_semihosting;
+extern crate cortex_m_rt;
+use cortex_m_rt::entry;
+
+//#[allow(unused_imports)]
+extern crate stm32f0;
+use stm32f0::stm32f0x1;
+
+mod fifo;
+
+//struct Fifo<T: ?Sized>{
+//	max_size: usize,
+//	data: T,
+//}
+//
+//
+//
+//macro_rules! make_fifo {
+//    ($size:literal) => {{
+//		Fifo{
+//			max_size: $size,
+//			data: [0; $size +1],
+//		}        
+//    }}
+//}
+//
+//const fn ff(ss: usize) -> Fifo{
+//	make_fifo!(ss)
+//}
+
+
+//extern crate stm32-rs;
+//use panic_halt; // When a panic occurs, stop the microcontroller
+
+// This marks the entrypoint of our application. The cortex_m_rt creates some
+// startup code before this, but we don't need to worry about this
+#[entry]
+fn main() -> ! {
+
+	
+    //let dp = pac::Peripherals::take().unwrap();
+	//let cp = cortex_m::Peripherals::take().unwrap();
+	let periph = stm32f0x1::Peripherals::take().unwrap();
+
+	let gpioc = &periph.GPIOC;
+//	let rcc = &periph.RCC;
+//	rcc.apb2enr.write(|w| w.iopcen().set_bit());
+//    gpioc.crh.write(|w| unsafe{
+//        w.mode13().bits(0b11);
+//        w.cnf13().bits(0b00)
+//    });
+	
+//	let aaa: Fifo<[u8; 30]> = Fifo{
+//		max_size: 30,
+//		data: [0; 30],
+//	};
+	
+	//let ff = make_fifo!(20);
+
+	let ff = fifo::Fifo2::new();
+		
+	
+    loop {
+        gpioc.bsrr.write(|w| w.bs13().set_bit());
+        //cortex_m::asm::delay(2000000);
+        gpioc.brr.write(|w| w.br12().set_bit());
+        //cortex_m::asm::delay(2000000);
+    }
+}
